f61224b059db16453ecc5e43fcd24aa47866d54b
Pedro Nieto <pedronieto.2005@gmail.com>
2022-10-20 14:03:20 -0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -46,3 +46,26 @@ void loop()
     delay(1000);
   }
 }
+
+//---------------------------------------------//#define pino_sinal_analogico A0
+int valor_analogico;
+
+#include <Servo.h>
+Servo servo;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(pino_sinal_analogico, INPUT);
+
+  servo.attach();
+  servo.write(0);
+}
+
+void loop(){
+  valor_analogico = analogRead(pino_sinal_analogico);
+  Serial.print(valor_analogico);
+
+  i(valor_analogico >= 800){
+    servo.wrie(180);
+  }
+}
