8bfe2a8f325906073b974635a355d66ea551cc05
Pedro Nieto <pedronieto.2005@gmail.com>
2022-11-01 07:49:33 -0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,37 +1,3 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,2);
-
-int valorAnalogico = 799;
-void setup()
-{
-  pinMode(2, OUTPUT);
-}
-
-void loop()
-{
-  lcd.setBacklight(HIGH);
-
-  if(valorAnalogico > 0 && valorAnalogico < 400){    
-    lcd.init();
-    lcd.setCursor(0,0);
-    lcd.print("Solo seco");
-    delay(1000);
-  }
-  else if(valorAnalogico > 400 && valorAnalogico < 800){
-    lcd.init();
-    lcd.setCursor(0,0);
-    lcd.print("Solo semi-umido");
-    delay(1000);
-  }
-  else {
-    lcd.init();
-    lcd.setCursor(0,0);
-    lcd.print("Solo umido");
-    delay(1000);
-  }
-}
-//---------------------------------------------//
 #define pino_sinal_analogico A0
 int valor_analogico;
 
@@ -41,7 +7,6 @@ Servo servo;
 void setup(){
   Serial.begin(9600);
   pinMode(pino_sinal_analogico, INPUT);
-
   servo.attach();
   servo.write(0);
 }
